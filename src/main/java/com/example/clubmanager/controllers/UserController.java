167ec3c45044af3e8a6ff2dfeb254ff6d8d33f1f
SRdA <sceuba@gmail.com>
2022-10-16 16:45:08 +0200

    second commit

--- src/main/java/com/example/clubmanager/controllers/UserController.java
+++ src/main/java/com/example/clubmanager/controllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/ClubManager/user")
 public class UserController {
